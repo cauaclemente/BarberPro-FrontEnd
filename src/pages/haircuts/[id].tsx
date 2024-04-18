08e93cd192818fc6e3978db583cf333af83807ad
Cauã Clemente <cauaclemente07@gmail.com>
2024-04-18 19:41:44 -0300

    Resgatando dados - pagina editar corte

--- src/pages/haircuts/[id].tsx
+++ src/pages/haircuts/[id].tsx
@@ -5,11 +5,36 @@ import { Flex, Text, Button, Heading, useMediaQuery, Input, Stack, Switch } from
 
 import { Sidebar } from "@/components/sidebar";
 import { FiChevronLeft } from "react-icons/fi";
+import { canSSRAuth } from "../../../utils/canSSRAuth";
+import { setUpAPIClient } from "@/service/api";
+import { useState } from "react";
 
 
-export default function EditHaircut(){
+interface HaircutProps{
+  id: string;
+  name: string;
+  price: string | number;
+  status: boolean;
+  user_id: string;
+}
+
+interface SubscriptionProps{
+  id: string;
+  status: string;
+}
+
+interface EditHaircutProps{
+  haircut: HaircutProps;
+  subscription: SubscriptionProps | null;
+}
+
+export default function EditHaircut({subscription, haircut}: EditHaircutProps){
 
   const [isMobile] = useMediaQuery("(max-width: 640px)")
+
+  const [name, setName] = useState(haircut?.name)
+  const [price, setPrice] = useState(haircut?.price)
+  const [status, setStatus] = useState(haircut?.status)
 
   return(
     <>
@@ -56,7 +81,8 @@ export default function EditHaircut(){
                 mb={3}
                 size="lg"
                 type="text"
-
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <Input 
                 placeholder="Valor do corte "
@@ -66,7 +92,8 @@ export default function EditHaircut(){
                 mb={5}
                 size="lg"
                 type="number"
-
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
               />
               <Stack mb={6} alignItems="center" direction="row">
                 <Text fontWeight="bold"> Desativar Corte </Text>
@@ -75,9 +102,27 @@ export default function EditHaircut(){
                   colorScheme="red"
                 />
               </Stack>
-              <Button mb={6} w="100%" bg="button.cta" color="gray.900" _hover={{ bg: "#ffb13e"}}>
+              <Button 
+                mb={6} 
+                w="100%" 
+                bg="button.cta" 
+                color="gray.900"
+                 _hover={{ bg: "#ffb13e"}} 
+                 isDisabled={subscription?.status !== 'active'}>
                 Salvar
               </Button>
+              {subscription?.status !== "active" && (
+                <Flex direction="row" align="center" justify="center">
+                  <Link href="/planos">
+                    <Text cursor="pointer" fontWeight="bold" color="#31fb6a" mr={1}>
+                      Seja premium
+                    </Text>
+                  </Link>
+                    <Text>
+                      e tenha todos acessos liberados.
+                    </Text>
+                </Flex>
+              )}
             </Flex>
           </Flex>
         </Flex>
@@ -85,3 +130,34 @@ export default function EditHaircut(){
     </>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const { id } = ctx.params;
+
+  try{
+    const apiClient = setUpAPIClient(ctx);
+
+    const check = await apiClient.get('/haircut/check');
+
+    const response = await apiClient.get('/haircut/detail',{
+      params:{
+        haircut_id: id,
+      }
+    })
+
+    return{
+      props:{
+        haircut: response.data,
+        subscription: check.data?.subscriptions
+      }
+    }
+
+  }catch{
+    return{
+      redirect:{
+        destination: '/haircuts',
+        permanent: false
+      }
+    }
+  }
+})
